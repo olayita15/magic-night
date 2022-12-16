6cd191a1e9bc3664359c623c181ab251a3ca18a3
Chris <cgolayac@correo.udistrital.edu.co>
2022-12-16 17:21:06 -0500

    Estructura de carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { OurComponent } from './pages/our/our.component';
+import { MarketComponent } from './pages/market/market.component';
+import { CardProductComponent } from './elements/card-product/card-product.component';
+import { CardCategoryComponent } from './elements/card-category/card-category.component';
+import { HeaderComponent } from './sections/header/header.component';
+import { FooterComponent } from './sections/footer/footer.component';
+import { CategoryComponent } from './sub-pages/category/category.component';
+import { ProductComponent } from './sub-pages/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    OurComponent,
+    MarketComponent,
+    CardProductComponent,
+    CardCategoryComponent,
+    HeaderComponent,
+    FooterComponent,
+    CategoryComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/our/our.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our',
+  templateUrl: './our.component.html',
+  styleUrls: ['./our.component.scss']
+})
+export class OurComponent {
+
+}
